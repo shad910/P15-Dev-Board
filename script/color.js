b4673aa58cd119ab08432f68a828f1ba67635e51
shad910 <shadshs91@gmail.com>
2025-03-01 23:58:55 +0600

    Added some funtions

--- script/color.js
+++ script/color.js
@@ -1,4 +1,4 @@
-const colors = ["bg-green-500", "bg-blue-500", "bg-red-500", "bg-violet-500", "bg-purple-500",  "bg-pink-500", "bg-orange-500", "bg-yellow-400"];
+const colors = ["bg-green-500", "bg-blue-500", "bg-red-500", "bg-violet-500", "bg-purple-500",  "bg-pink-500", "bg-orange-500", "bg-yellow-400", "bg-slate-400"];
 
 let colorIndex = 0;
 
